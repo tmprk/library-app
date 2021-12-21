2bfb50a1b6c8440527d57008fa4fc847b7c0fb45
yogurtsake <parkimothy@gmail.com>
2021-12-20 21:32:26 -0800

    validate form input

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var modal = document.getElementById('modal');
+const table = document.getElementById('book-list');
+const modal = document.getElementById('modal');
 
 // Get the button that opens the modal
-var addBtn = document.getElementById("add");
+const addBtn = document.getElementById("add");
 
-// Get the <span> element that closes the modal
-var span = document.getElementById("cancel");
+// Get the cancel that closes the modal
+const cancel = document.getElementById("cancel");
+
+const bookName = document.getElementById('bname');
+const pages = document.getElementById('pages');
+const didRead = document.getElementById('read')
+const errorMsg = document.getElementById('error');
+const submitButton = document.getElementById('submit');
+
+function closeModal() {
+    modal.style.display = "none";
+    document.body.classList.remove('modal-open');
+}
 
 // When the user clicks on the button, open the modal
 addBtn.onclick = () => {
@@ -13,17 +25,43 @@ addBtn.onclick = () => {
 }
 
 // When the user clicks on cancel, close the modal
-span.onclick = () => {
-    modal.style.display = "none";
-    document.body.classList.remove('modal-open');
-    document.body.style.overflow = 'auto';
+cancel.onclick = () => {
+    closeModal()
+    // document.body.style.overflow = 'auto';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = (event) => {
     if (event.target == modal) {
-        modal.style.display = "none";
-        document.body.classList.remove('modal-open');
-        document.body.style.overflow = 'auto';
+        closeModal();
+        // document.body.style.overflow = 'auto';
     }
 }
+
+function validate(e) {
+    if (e.target.value == null || e.target.value == '') {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+    }
+}
+
+bookName.addEventListener('input', validate);
+
+submitButton.onclick = function () {
+    if (bname.value && pages.value) {
+        console.log('all validated')
+        console.log(bookName.value, pages.value, didRead.value);
+
+        // insert row here
+        // let newRow = table.insertRow(-1);
+        // Insert a cell in the row at index 0
+        // let newCell = newRow.insertCell(0);
+        // Append a text node to the cell
+        // let newText = document.createTextNode('New bottom row');
+        // newCell.appendChild(newText);
+        closeModal();
+    } else {
+        console.log('not validated')
+    }
+};
